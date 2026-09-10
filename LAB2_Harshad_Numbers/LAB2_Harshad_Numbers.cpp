b9ffb6e44d5de6a0c752228a9a6d634c675d3ec5
Elie Kalouche <116683442+Ekalouche@users.noreply.github.com>
2026-09-10 14:36:50 -0400

    Lab2 initialization

---
+++ LAB2_Harshad_Numbers/LAB2_Harshad_Numbers.cpp
@@ -0,0 +1,20 @@
+// LAB2_Harshad_Numbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    /* Get maximum number */
+    Basename = GetFilename();
+    /* Loop through reading all the lines of the file “Basename.in” */
+    /* Check to see if the number is an Harshad number or not */
+    if (IsHarshad(CandiateNumber) == 1)
+    {
+        /* Indicate a Harshard number in the output file “Basename.out”*/
+    } /* if() */
+    else
+    {
+        /* Indicate NOT a Harshard number in the output file “Basename.out” */
+    } /* if() */
+} /* loop
