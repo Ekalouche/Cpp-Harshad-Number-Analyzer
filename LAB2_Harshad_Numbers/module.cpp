529f6ad659522b471872317ad6083f7796abcda2
Elie Kalouche <116683442+Ekalouche@users.noreply.github.com>
2026-09-10 15:15:06 -0400

    V1 not done

---
+++ LAB2_Harshad_Numbers/module.cpp
@@ -0,0 +1,36 @@
+
+//Modual file for LAB2_Harshaad_Numbers
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int IsHarshad(int value)
+{
+	
+}
+
+void GetFilename()
+{
+	string file_Name;
+	int number;
+
+	cout << "Please input a file name"
+	cin >> file_Name;
+
+	ifstream file1(file_Name);
+
+	if (file1.is_open())
+	{
+		
+
+	}
+	else
+	{
+
+	}
+	
+	while(file1 >> )
+
+}
