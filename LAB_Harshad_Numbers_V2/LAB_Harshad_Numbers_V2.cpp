62c79ce7a98851825eba158ce3fde072b37a4994
Elie Kalouche <Ekalouche111@gmail.com>
2026-09-11 01:06:58 -0400

    V4 not running

---
+++ LAB_Harshad_Numbers_V2/LAB_Harshad_Numbers_V2.cpp
@@ -0,0 +1,30 @@
+// LAB2_Harshad_Numbers.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "module.h"
+using namespace std;
+
+int main()
+{
+    string baseName = GetFilename();
+
+    ifstream inFile(baseName + ".in");
+    ofstream outFile(baseName + ".out");
+
+    int number;
+    while (inFile >> number)
+    {
+        if (IsHarshad(number) == 1)
+        {
+            outFile << number << " is a Harshad number" << endl;
+        } // if()
+        else
+        {
+            outFile << number << " is NOT a Harshad number" << endl;
+        } // if()
+    } // loop()
+
+    return 0;
+}
---
+++ LAB_Harshad_Numbers_V3/Module.h
@@ -0,0 +1,13 @@
+#pragma once
+// module.h
+
+#ifndef MODULE_H
+#define MODILE_H
+#include <string>
+
+using namespace std;
+
+string GetFilename();
+int IsHarshad(int value);
+
+#endif#pragma once
